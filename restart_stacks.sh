87be9b3bcbca5fbd514f60bffc675fb585dfb0eb
eehunt <ethan@hunthome.dev>
2024-12-27 12:52:19 +0000

    Bash script to stop/start all stacks in the appropriate order (#3)
    Quick little script to assist in deployment of docker-compose changes

---
+++ restart_stacks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DOCKER_ROOT="/srv/docker"
+
+# stack directories; keep reverse proxy last!
+SUB_DIRS=("ghost" "it-tools" "portainer" "nginx-proxy-manager")
+
+# stop all stacks
+for SUB_DIR in "${SUB_DIRS[@]}"; do
+    echo "stopping ${SUB_DIR}"
+    cd "${DOCKER_ROOT}/${SUB_DIR}" || {
+        echo "Failed to change to ${SUB_DIR} directory"
+        exit 1
+    }
+    sudo docker compose down
+done
+
+# restart stacks in reverse order they were stopped
+for ((i=${#SUB_DIRS[@]}-1; i>=0; i--)); do
+    SUB_DIR="${SUB_DIRS[i]}"
+    echo "starting ${SUB_DIR}"
+    cd "${DOCKER_ROOT}/${SUB_DIR}" || {
+        echo "Failed to change to ${SUB_DIR} directory"
+        exit 1
+    }
+    sudo docker compose up -d
+done
